ae6d8eb0b0fcbbac696ccc2c8a5c9019b442353d
wn-jasondeng <jasondeng@vattensoft.com>
2020-08-19 16:23:56 +0800

    track files

---
+++ AspNetCorePdfExample/Pages/PdfDownload.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Wkhtmltopdf.NetCore;
+
+namespace AspNetCorePdfExample
+{
+    public class PdfDownloadModel : PageModel
+    {
+        readonly IGeneratePdf generatePdf;
+
+        public PdfDownloadModel(IGeneratePdf generatePdf)
+        {
+            this.generatePdf = generatePdf;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            return await generatePdf.GetPdf("ViewTemplates/PdfHtmlTemplate.cshtml", "hello world!");
+        }
+    }
+}
---
+++ AspNetCorePdfExample/Startup.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Wkhtmltopdf.NetCore;
+
+namespace AspNetCorePdfExample
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            Environment = env;
+        }
+
+        
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+
+            /* Windows path*/
+            var winPath = Path.Combine(Environment.ContentRootPath, @"../Binary/wkhtmltopdf");
+            /* Linux path */
+            //var linuxStagingPath = "/var/www/Binary/wkhtmltopdf";
+
+            services.AddWkhtmltopdf(winPath);
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
